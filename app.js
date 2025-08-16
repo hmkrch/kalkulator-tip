4bf2a0fc93a4f18d3c0ac34bd60a41f09f18f533
Hamka Rachman <hamkaet@gmail.com>
2025-08-17 02:07:13 +0900

    update code js

--- app.js
+++ app.js
@@ -1,54 +1,86 @@
-"use strcit";
+const tampilkanHasil = document.querySelector("h1");
 
-// Variabel
-const hargaInp = document.getElementById("hargaInp");
-const tipInp = document.getElementById("tipInp");
+const inputHarga = document.getElementById("inputHarga");
+const inputTip = document.getElementById("inputTip");
 
-const inputs = document.querySelectorAll("input");
+const buttonPlus = document.getElementById("plus");
+const buttonMin = document.getElementById("minus");
 
-const tampikanTotal = document.getElementById("tampilkanTotal");
+const numberButton = document.querySelector("p");
+let number = Number(numberButton.textContent);
 
-const kurang = document.getElementById("kurang");
-const tambah = document.getElementById("tambah");
+const clearBtn = document.getElementById("clearBtn");
 
-const hasilTambahKurang = document.getElementById("hasilTambahKurang");
+// console.log(typeof number, number);
 
-// Function
+const hitung = () => {
+  const hargaStr = inputHarga.value;
 
-const tampilkanHarga = () => {
-  const harga = Number(hargaInp.value);
-  const tip = Number(tipInp.value);
-  const totalOrang = Number(hasilTambahKurang.textContent);
-
-  // if (harga) {
-  //   tampikanTotal.textContent = `Rp. ${harga.toFixed(3)}`;
-  // }
-
-  const total = harga + (harga * tip) / 100;
-  const perOrang = total / totalOrang;
-  // const totalFix = total / totalOrang;
-  tampikanTotal.textContent = `Rp. ${perOrang.toFixed(3)}`;
-
-  return perOrang;
-};
-
-// console.log(typeof tampilkanHarga());
-
-hargaInp.oninput = () => tampilkanHarga();
-tipInp.oninput = () => tampilkanHarga();
-
-kurang.onclick = () => {
-  let number = Number(hasilTambahKurang.textContent);
-  if (number > 1) {
-    hasilTambahKurang.textContent = number - 1;
-    tampilkanHarga();
+  if (hargaStr === "") {
+    return `Tidak Boleh Kosong`;
   }
+
+  const tipNum = Number(inputTip.value);
+  const hargaNum = Number(hargaStr);
+  if (isNaN(hargaNum) || isNaN(tipNum)) {
+    return `Harus berisi Angka`;
+  }
+
+  const hargaWithTip = (tipNum / 100) * hargaNum;
+  const totalHarga = hargaWithTip + hargaNum;
+
+  return totalHarga;
 };
 
-tambah.onclick = () => {
-  let number = Number(hasilTambahKurang.textContent);
-  hasilTambahKurang.textContent = number + 1;
-  tampilkanHarga();
-
-  // console.log(fix);
+const tambah = () => {
+  numberButton.textContent = ++number;
+  return number;
 };
+
+const kurang = () => {
+  if (number > 1) {
+    numberButton.textContent = --number;
+  }
+
+  return number;
+};
+const bagi = () => {
+  const total = hitung();
+
+  if (typeof total === "string") {
+    return total;
+  }
+
+  return (total / number).toFixed(3);
+};
+
+const clear = () => {
+  number = 1;
+  inputHarga.value = "";
+  inputTip.value = "";
+  numberButton.textContent = 1;
+  tampilkanHasil.textContent = "";
+  inputHarga.focus();
+};
+
+clearBtn.onclick = () => {
+  clear();
+};
+
+[inputHarga, inputTip].forEach((input) => {
+  input.addEventListener("keyup", () => {
+    tampilkanHasil.textContent = bagi();
+  });
+});
+
+[buttonPlus, buttonMin].forEach((button) => {
+  button.addEventListener("click", () => {
+    if (button.textContent === "+") {
+      tambah();
+      tampilkanHasil.textContent = bagi();
+    } else {
+      kurang();
+      tampilkanHasil.textContent = bagi();
+    }
+  });
+});
